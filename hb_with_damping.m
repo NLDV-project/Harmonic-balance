4c0ebe6d4496b98f82c88dbbca2db46425480604
ranjith263 <addagalla.2@wright.edu>
2015-12-11 04:32:16 -0500

    linear harmonic balance with damping
    I have added the damping term in the equation and accordingly I have
    taken particular solution.  Please find and provide the details for
    modifications, if any

---
+++ hb_with_damping.m
@@ -0,0 +1,38 @@
+%equation x"+x'+x=sinwt
+function hb_with_damping()
+clc
+clear all
+close all
+global N T w t F W X
+N=100;
+T=2;
+w=2*pi/T;
+t=linspace(0,T,N+1);%actually they will be N equally divided points in continuous system
+t=t(1:end-1);%we required N-1 points in the current system
+F=sin(w*t);%forcing term
+iw=(0:ceil(N-1)/2)*1i*(w);
+miw=(-1i)*(floor(N/2):-1:1)*(w);
+W=[iw,miw];
+X=fft(F);
+x=fminsearch(error(), X);
+
+function residue=error()
+dotX=W.*X;
+dotx=ifft(dotX);
+ddotX=(W.^2).*X;
+ddotx=ifft(ddotX);
+x0=ddotx/W.^2;
+
+residue=@(X) sum(abs(ddotx+dotx+x0-F).^2)
+
+end
+ figure(1)
+ a=w*sqrt(1-(ifft(x)).^2)-(ifft(x)).*(1-w^2);
+ b=w*sqrt(1-F.^2)-F.*(1-w^2);
+ plot(t,a./(1-2*w^2),t,b./(1-2*w^2),'*-')
+ x_0=0;v0=0;
+ fnc = @(t,x)[x(2);sin(w*t)-x(2)-x(1)]
+ [tspan,xval] = ode45(fnc,t,[x_0 v0])
+ figure(2)
+ plot(t,xval(:,1),t,ifft(x)/(1-w^2),'o-')
+end
